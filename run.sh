f3358c2b3d4124700d909b948eb1442339809330
foxsir <contact@umbst.com>
2023-05-03 14:01:18 +0800

    feat: add text color

--- run.sh
+++ run.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
+GREEN=`tput setaf 40`
+BLUE=`tput setaf 45`
+
+clear
+
+echo -e "$GREEN";
+echo "--------"
+echo "defaulted run base part"
+echo "param A or a, to run advanced part"
+echo "--------"
+echo -e "$GREEN";
+
+sleep 1
+
 if [[ $1 = 'A' || $1 = 'a' ]]
 then
+  echo -e "${BLUE}run advanced part"
   gradle advanced:run
 else
+  echo "${BLUE}run base part"
   gradle base:run
 fi
